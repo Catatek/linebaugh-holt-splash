d3cd82eab10e3dc4815a8e52e4d1729f6aa5cb7d
JSLinn727 <justinslinn727@gmail.com>
2017-10-20 17:37:36 -0400

    Added Index and attached to components

---
+++ src/components/People.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="people">
+  <div class="itemContainer">
+    <img src="" alt="">
+    <span>Justin Linn</span>
+    <img src="" alt="">
+    <span></span>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'People',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.people {
+
+}
+
+</style>
